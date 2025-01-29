3a6c2b1013765460eb7ae998ac37f41d89d29ec0
Goncalo065 <goncaloabreu.sa@hotmail.com>
2025-01-29 12:10:46 +0000

    javascript alteração

--- js/loja.js
+++ js/loja.js
@@ -1,1 +1,9 @@
 
+const btn= document.querySelectorAll(".AddButton")
+
+btn.forEach(button=> {
+    button.addEventListener('click', () => {
+        alert("ADICIONADO AO CARRINHO");
+    });
+});
+
