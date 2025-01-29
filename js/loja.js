22dfb46b6f49b2adca45feb1e29f33806009d676
Goncalo065 <goncaloabreu.sa@hotmail.com>
2025-01-29 16:39:00 +0000

    carrinho

--- js/loja.js
+++ js/loja.js
@@ -1,9 +1,55 @@
+let cart = [];
 
-const btn= document.querySelectorAll(".AddButton")
+function addToCart(productName, productPrice) {
+    let product = {
+        name: productName,
+        price: productPrice
+    };
 
-btn.forEach(button=> {
-    button.addEventListener('click', () => {
-        alert("ADICIONADO AO CARRINHO");
+    cart.push(product);
+    updateCart();
+}
+
+
+function updateCart() {
+    const cartItemsContainer = document.getElementById("Items");
+    const totalPriceElement = document.getElementById("totalPrice");
+
+    cartItemsContainer.innerHTML = "";
+
+
+    let total = 0;
+    cart.forEach((item, index) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = `${item.name} - $${item.price}`;
+
+
+        const removeButton = document.createElement("button");
+        removeButton.textContent = "Remove";
+        removeButton.onclick = function () {
+            removeFromCart(index);
+        };
+        listItem.appendChild(removeButton);
+
+        cartItemsContainer.appendChild(listItem);
+
+        total += item.price;
     });
-}); 
+
+    
+    totalPriceElement.textContent =`Total: $${total}`;
+}
+
+
+function removeFromCart(index) {
+    cart.splice(index, 1);
+    updateCart();
+}
+
+
+function clearCart() {
+    cart = [];
+    updateCart();
+}
+
 
